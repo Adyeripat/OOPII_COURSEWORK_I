9a614e1313d950d6c2d4c62b78c938bdb8478e04
Patrick Kahango <percylove@hilarious.com>
2019-02-23 12:31:35 +0300

    oop project coursework

---
+++ src/oop2_coursework01/OOP2_CourseWork01.java
@@ -0,0 +1,208 @@
+package oop2_coursework01;
+
+import java.util.Scanner;
+
+
+public class OOP2_CourseWork01 {
+
+    public static void main(String[] args) {
+ int mark[] = new int [7];
+     int m = 0;
+     String grd;
+      float sum=0;
+     Scanner scan = new Scanner(System.in);
+     System.out.println("Input Student Name");
+     String  S=scan.next();
+     System.out.println(S);
+      
+     System.out.println("Input 1st Course Unit Name");
+     String  R=scan.next();
+     System.out.println( R);
+     
+     System.out.println("Input Marks");
+               
+      mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+     
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+    }
+     
+     System.out.println("Input 2nd Course Unit Name");
+     String  T=scan.next();
+     System.out.println(T);
+     
+     System.out.println("Input Marks");
+     
+     mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+                             
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+    }
+     
+     System.out.println("Input 3rd Course Unit Name");
+     String  Q=scan.next();
+     System.out.println(Q);
+     
+     System.out.println("Input Marks");
+    mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+         
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+    }
+     
+     System.out.println("Input 4th Course Unit Name");
+     String  V=scan.next();
+     System.out.println(V);
+     
+     System.out.println("Input Marks");
+     mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+         
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+    }     
+     
+     System.out.println("Input 5th Course Unit Name");
+     String  W=scan.next();
+     System.out.println(W);
+     
+     System.out.println("Input Marks");
+     mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+         
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+    }
+     
+     System.out.println("Input 6th Course Unit Name");
+     String  X=scan.next();
+     System.out.println(X);
+     
+     System.out.println("Input Marks");
+    mark[m] = scan.nextInt();
+         sum = sum + mark[m];
+         
+     if(mark[m]>=80 && mark[m]<=100){
+         grd="A";
+         System.out.println("A");
+        }
+     else if(mark[m]>70 && mark[m]<=79){
+         grd="B";
+         System.out.println("B");
+     }
+     else if(mark[m]>=60 && mark[m]<=69){
+         grd="C";
+         System.out.println("C");
+     }
+     else if(mark[m]>=50 && mark[m]<59){
+         grd="D";
+         System.out.println("D");
+     }
+     else if(mark[m]>40 && mark[m]<=49){
+         System.out.println("F");
+     }
+     else if(mark[m]<=39){
+        System.out.println("Null");
+     }
+     
+    }
+    
+}
